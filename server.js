dc96f5a7b2e7283158f572bee28fa3dcd674f866
akashkarthi <rvnath2006@gmail.com>
2017-02-02 22:28:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/articleone'),function (req,res){res.sendfile(article1.html)
+}
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
